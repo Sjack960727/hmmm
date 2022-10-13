3ebe286df2b5d5b3ec14e9c6cde6b6ada40a419f
sjk <649937897@qq.com>
2022-10-13 16:11:31 +0800

    1611

--- src/module-hmmm/pages/questions-new.vue
+++ src/module-hmmm/pages/questions-new.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class='container'>试题录入</div>
+  <div class="container">
+    <el-card>
+      <div slot="header">
+        <span>试题录入</span>
+      </div>
+      <div>学科***</div>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container {
+  padding: 20px;
+}
+</style>
