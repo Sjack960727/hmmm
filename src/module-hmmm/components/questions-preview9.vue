0b8db9baf359717398249acafba80771565605d3
sjk <649937897@qq.com>
2022-10-13 16:03:22 +0800

    1603

---
+++ src/module-hmmm/components/questions-preview9.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class='container'>
+    <el-dialog
+  title="题目预览"
+  :visible="dialogVisible"
+  width="60%"
+  :before-close="handleClose">
+  <div>
+    <el-row>
+      <el-col :span="6" style="padding:0 10px;height:36px;"><div>【题型】：{{ questionDetail.questionType|formatQuestionType}}</div></el-col>
+      <el-col :span="6" style="padding:0 10px;height:36px;"><div>【编号】：{{ questionDetail.id}}</div></el-col>
+      <el-col :span="6" style="padding:0 10px;height:36px;"><div>【难度】：{{ questionDetail.difficulty|formatDifficulty}}</div></el-col>
+      <el-col :span="6" style="padding:0 10px;height:36px;"><div>【标签】：{{ questionDetail.tags }}</div></el-col>
+      <el-col :span="6" style="padding:0 10px;height:36px;"><div>【学科】：{{ questionDetail.subjectName }}</div></el-col>
+      <el-col :span="6" style="padding:0 10px;height:36px;"><div>【目录】：{{ questionDetail.directoryName}}</div></el-col>
+      <el-col :span="6" style="padding:0 10px;height:36px;"><div>【方向】：{{ questionDetail.direction}}</div></el-col>
+    </el-row>
+  </div>
+  <hr>
+  <div>【题干】： <div style="color:blue;" v-html="questionDetail.question"></div>
+  <div> 单选题 选项：（以下选中的选项为正确答案）
+    题目xxx未完成，判断单选多选，此处应该v-if
+  </div>
+  <div> 多选题 选项：（以下选中的选项为正确答案）
+
+  </div>
+  <div> 简答题 选项：（以下选中的选项为正确答案）
+
+  </div>
+  </div>
+
+  <hr>
+ 【参考答案】： <el-button type="danger" @click="showVideo=true">视频答案预览</el-button>
+<div class="video" v-show="showVideo"> <video controls='controls' :src="questionDetail.videoURL"></video></div>
+
+  <hr>
+  <div>【答案解析】: <div style="display: inline-block;" v-html="questionDetail.answer"></div></div>
+
+  <hr>
+ 【题目备注】:  {{ questionDetail.remarks }}
+
+  <span slot="footer" class="dialog-footer">
+    <el-button type="primary" @click="handleClose">关闭</el-button>
+  </span>
+</el-dialog>
+    </div>
+</template>
+
+<script>
+// import { questionType, difficulty } from '@/api/hmmm/constants.js'
+export default {
+  props: {
+    dialogVisible: {
+      type: Boolean,
+      default: false
+    }
+
+  },
+  data () {
+    return {
+      questionDetail: {},
+      showVideo: false
+    }
+  },
+  created () {
+  },
+  methods: {
+    handleClose (done) {
+      this.$emit('update:dialogVisible', false)
+    },
+    formatterFn () {
+      return 1
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.video {
+    width: 400px;
+    height: 300px;
+    video{
+      width: 100%;
+      height: 100%;
+    }
+  }
+  </style>
--- src/module-hmmm/pages/questions-randoms.vue
+++ src/module-hmmm/pages/questions-randoms.vue
@@ -1,9 +1,194 @@
 <template>
-  <div class='container'>题组列表sjk</div>
+  <div class="container">
+    <el-card class="box-card">
+      <!-- 搜索 -->
+      <el-form :inline="true" :model="formData" class="demo-form-inline">
+        <el-row type="flex" justify="end">
+          <el-col :span="12">
+            <el-form-item label="关键词">
+              <el-input
+                placeholder="根据编号搜索"
+                v-model="formData.keywords"
+              ></el-input> </el-form-item
+          ></el-col>
+          <el-col :span="12"
+            ><el-form-item class="btn">
+              <el-button @click="formData.keywords = ''">清除</el-button>
+              <el-button type="primary" @click="goSearch">搜索</el-button>
+            </el-form-item></el-col
+          >
+        </el-row>
+      </el-form>
+      <!-- 警告框 -->
+      <el-alert :title="alertTitle" type="info" show-icon> </el-alert>
+      <!-- 表格 -->
+      <el-table v-loading="loading" style="width: 100%" :data="randomsList">
+        <el-table-column prop="id" label="编号" width="220"> </el-table-column>
+        <el-table-column
+          prop="questionType"
+          label="题型"
+          width="80"
+          :formatter="formatterFn"
+        >
+        </el-table-column>
+        <el-table-column prop="questionIDs" label="题目编号" width="220">
+          <template slot-scope="{ row }">
+            <div v-for="item in row.questionIDs" :key="item.number">
+              <a
+                href=""
+                style="color: #0099ff"
+                @click.prevent="getDetails(item)"
+                >{{ item.number }}</a
+              >
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="addTime" label="录入时间" width="160">
+        </el-table-column>
+        <el-table-column prop="totalSeconds" label="答题时间(s)" width="165">
+        </el-table-column>
+        <el-table-column prop="accuracyRate" label="正确率(%)" width="165">
+        </el-table-column>
+        <el-table-column prop="userName" label="录入人" width="165">
+        </el-table-column>
+        <el-table-column prop="address" label="操作" width="80">
+          <template slot-scope="{ row }">
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              circle
+              plain
+              @click="deleteRandoms(row.id)"
+            ></el-button>
+            <!-- <el-button type="text" size="small" @click="deleteRandoms(row.id)">删除</el-button> -->
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页组件 -->
+      <el-pagination
+        class="page"
+        background
+        @size-change="getRandoms"
+        @current-change="getRandoms"
+        :current-page.sync="page.page"
+        :page-sizes="[20, 30, 50, 100]"
+        :page-size.sync="page.pagesize"
+        layout="prev, pager, next,sizes, jumper"
+        :total="total"
+      >
+      </el-pagination>
+    </el-card>
+    <!-- 弹出框 -->
+    <questionspreview
+      :dialog-visible.sync="dialogVisible"
+      ref='questionspreview'
+    ></questionspreview>
+  </div>
 </template>
 
 <script>
-export default {}
+import { randoms, removeRandoms, detail } from '@/api/hmmm/questions.js'
+import questionspreview from '../components/questions-preview9.vue'
+// 点击打开题库dialog组件
+export default {
+  components: {
+    questionspreview
+  },
+  data () {
+    return {
+      loading: false,
+      dialogVisible: false,
+      formData: {
+        keywords: ''
+      },
+      alertTitle: '数据一共0条',
+      currentPage4: 4,
+      page: {
+        page: 1,
+        pagesize: 20
+      },
+      total: 0,
+      randomsList: [],
+      questionDetail: {}
+    }
+  },
+  created () {
+    this.getRandoms()
+  },
+  methods: {
+    goSearch () {
+      this.page.keyword = this.formData.keywords
+      this.getRandoms()
+    },
+    async getDetails (item) {
+      // console.log(item.id)
+      const data = { id: item.id }
+      const res = await detail(data)
+      console.log(res.data)
+      this.questionDetail = res.data
+      this.$refs.questionspreview.questionDetail = { ...this.questionDetail }
+      this.dialogVisible = true
+    },
+    async getRandoms () {
+      this.loading = true
+      const { data } = await randoms(this.page)
+      this.loading = false
+      this.randomsList = data.items
+      this.total = data.counts
+      this.alertTitle = `数据一共${data.counts}条`
+      // console.log(data)
+    },
+    deleteRandoms (id) {
+      this.$confirm('此操作将永久删除该题组, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          const data = { id: id }
+          // console.log(data)
+          removeRandoms(data)
+        })
+        .then(() => {
+          this.getRandoms()
+        })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          })
+        })
+        .catch(() => {})
+    },
+    formatterFn (row, column, cellvalue) {
+      if (cellvalue === '1') {
+        return '单选'
+      }
+      if (cellvalue === '2') {
+        return '多选'
+      }
+      if (cellvalue === '3') {
+        return '简答'
+      }
+    }
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container {
+  padding: 0 10px;
+  margin: 10px 0;
+  .box-card {
+    // padding: 20px;
+    padding-bottom: 20px;
+    .btn {
+      float: right;
+    }
+    .page {
+      float: right;
+      margin-top: 20px;
+    }
+  }
+}
+</style>
