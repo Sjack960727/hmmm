d939cc05cf690597eef175e2f151ae4dd2fd6f94
sjk <649937897@qq.com>
2022-10-14 12:14:43 +0800

    1

--- src/module-hmmm/pages/questions-new.vue
+++ src/module-hmmm/pages/questions-new.vue
@@ -4,13 +4,57 @@
       <div slot="header">
         <span>试题录入</span>
       </div>
-      <div>学科***</div>
+      <div>
+        <el-form ref="form" :model="formData" label-width="80px">
+          <el-form-item label="学科"> </el-form-item>
+          <el-form-item label="学科">
+            <el-select v-model="value" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      formData: {},
+      options: [
+        {
+          value: '选项1',
+          label: '黄金糕'
+        },
+        {
+          value: '选项2',
+          label: '双皮奶'
+        },
+        {
+          value: '选项3',
+          label: '蚵仔煎'
+        },
+        {
+          value: '选项4',
+          label: '龙须面'
+        },
+        {
+          value: '选项5',
+          label: '北京烤鸭'
+        }
+      ],
+      value: ''
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
