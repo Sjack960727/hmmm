865f1e0d5096a24caf19b20638a211bed0ffeb99
mao <2041353520@qq.com>
2022-10-14 18:22:05 +0800

    feat:目录页面差一个修改功能

--- src/module-hmmm/components/directorys-add.vue
+++ src/module-hmmm/components/directorys-add.vue
@@ -1,9 +1,74 @@
 <template>
-  <div class='container'>添加目录对话框</div>
+  <div class="container">
+    <el-dialog title="新增目录" :visible.sync="dialogVisible" width="30%" :before-close="cancel">
+      <el-form ref="form" :model="form" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="所属学科" >
+            <el-select placeholder="请选择" v-model="condition" style="width:100%" @change="selecChange">
+                <el-option :label="item.label" :value="item"
+                v-for="item in addLists"
+                :key="item.value">
+              </el-option>
+              </el-select>
+          </el-form-item>
+        <el-form-item label="目录名称"  prop="directoryName" :rules="[{required:true,trigger:'blur',message:'请输入目录名称'}]">
+          <el-input v-model="form.directoryName"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cancel">取 消</el-button>
+        <el-button type="primary" @click="submit">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-export default {}
+import { simple } from '@/api/hmmm/subjects.js'
+import { add } from '@/api/hmmm/directorys.js'
+export default {
+  props: {
+    dialogVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      addLists: [],
+      form: {
+        directoryName: '',
+        subjectID: '',
+        id: null
+      },
+      condition: ''
+    }
+  },
+  created () {
+    this.addList()
+  },
+  methods: {
+    async addList () {
+      const { data } = await simple()
+      this.addLists = data
+      // console.log(data)
+    },
+    selecChange (e) {
+      console.log(e.value)
+      this.form.subjectID = e.value
+    },
+    async submit () {
+      await this.$refs.form.validate()
+      console.log(123)
+      await add(this.form)
+      this.$parent.getTableList()
+      this.cancel()
+      this.$message.success('添加成功')
+    },
+    cancel () {
+      this.$emit('update:dialogVisible', false)
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'></style>
--- src/module-hmmm/pages/directorys.vue
+++ src/module-hmmm/pages/directorys.vue
@@ -10,20 +10,20 @@
         <el-col :span="15">
           <el-form-item label="状态">
             <el-select placeholder="请选择" v-model="condition">
-                <el-option label="启用" value="shanghai"></el-option>
-                <el-option label="禁用" value="beijing"></el-option>
+                <el-option label="启用" value="启用"></el-option>
+                <el-option label="禁用" value="禁用"></el-option>
               </el-select>
           </el-form-item>
           <el-form-item>
-            <el-button>清除</el-button>
-            <el-button type="primary">搜索</el-button>
+            <el-button @click="condition=''">清除</el-button>
+            <el-button type="primary" @click="search">搜索</el-button>
           </el-form-item>
         </el-col>
         <!-- <el-col>
         </el-col> -->
         <el-col :span="3">
           <div class="grid-content bg-purple-light">
-            <el-button type="success" icon="el-icon-edit">新增试题</el-button>
+            <el-button type="success" icon="el-icon-edit" @click="dialog">新增目录</el-button>
           </div>
         </el-col>
       </el-form>
@@ -54,24 +54,31 @@
               >{{scope.row.state==='已禁用'?'启用':'禁用'}}</el-button
             >
             <el-button type="text" :disabled="scope.row.state==='已启用'">修改</el-button>
-            <el-button type="text" :disabled="scope.row.state==='已启用'">删除</el-button>
+            <el-button type="text" :disabled="scope.row.state==='已启用'" @click="del(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
 
     </el-card>
+    <!-- 弹出框 -->
+    <directorys-add :dialogVisible.sync="isShow"></directorys-add>
   </div>
 </template>
 
 <script>
-import { list } from '@/api/hmmm/directorys.js'
+import { list, changeState, remove } from '@/api/hmmm/directorys.js'
 import { parseTimeByString } from '@/filters'
+import DirectorysAdd from '../components/directorys-add.vue'
 export default {
+  components: {
+    DirectorysAdd
+  },
   data () {
     return {
+      isShow: false,
       muluname: '',
       condition: '',
-      counts: 10,
+      counts: '',
       use: '',
       page: {
         page: 1,
@@ -81,21 +88,44 @@ export default {
     }
   },
   created () {
-    this.getTableList()
+    this.getTableList(this.page)
   },
   methods: {
-    async getTableList () {
-      const { data } = await list(this.page)
+    async getTableList (a) {
+      const { data } = await list(a)
+      this.counts = data.counts
       this.tableList = data.items
       this.tableList.forEach(item => {
         item.addDate = parseTimeByString(item.addDate)
         item.state = item.state === 0 ? '已禁用' : '已启用'
       })
     },
-    handleClick (row) {
+    async handleClick (row) {
+      // console.log(row)
       /* eslint-disable */
+      const data = {
+        id: row.id,
+        state: row.state = row.state === '已禁用' ? 1 : 0
+      }
+    await  changeState(data)
+      this.getTableList()
       row.state = row.state === '已禁用' ? '已启用' : '已禁用'
       this.$message.success('操作成功')
+    },
+    search() {
+      const state = this.condition === '禁用' ? 0 : 1
+      const searchdata = { state, ...this.page }
+      this.getTableList(searchdata)
+    //  console.log(searchdata);
+    },
+    dialog() {
+      console.log(123);
+      this.isShow=true
+    },
+   async del(row) {
+     await remove(row)
+     this.getTableList()
+     this.$message.success('删除成功')
     }
   }
 }
